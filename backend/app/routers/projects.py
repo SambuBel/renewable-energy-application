f9d7c51ecf8e87cb5e1ebb16e3b62d2efc06a1c4
BelSambu <bel.sambu@gmail.com>
2025-05-08 21:32:47 -0300

    dlte extra file and added more options to play with de api

--- backend/app/routers/projects.py
+++ backend/app/routers/projects.py
@@ -16,4 +16,26 @@ async def get_project(project_id: int):
     for project in SAMPLE_PROJECTS:
         if project.id == project_id:
             return project
-    return {"error": "Project not found"} 
+    return {"error": f"Project {project_id} not found"}
+
+## The following routes are saved in memory, not in a database
+@router.post("/projects", response_model=Project)
+async def create_project(project: Project):
+    SAMPLE_PROJECTS.append(project)
+    return project
+
+@router.put("/projects/{project_id}", response_model=Project)
+async def update_project(project_id: int, project: Project):
+    for i, p in enumerate(SAMPLE_PROJECTS):
+        if p.id == project_id:
+            SAMPLE_PROJECTS[i] = project
+            return project
+    return {"error": f"Project {project_id} not found"}
+
+@router.delete("/projects/{project_id}", response_model=Project)
+async def delete_project(project_id: int):
+    for i, p in enumerate(SAMPLE_PROJECTS):
+        if p.id == project_id:
+            return SAMPLE_PROJECTS.pop(i)
+    return {"error": f"Project {project_id} not found"}
+
